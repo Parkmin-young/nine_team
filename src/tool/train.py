463a5d3be13684c6c6d8aa0df31bcdbdcf2e8841
kcc5107 <kcc5107@naver.com>
2020-11-15 12:14:28 +0900

    train 코드 완성

--- src/tool/train.py
+++ src/tool/train.py
@@ -1,5 +1,13 @@
-model = loadModel()
-tx, ty, x, y = loadMemory()
-ret = model.fit(train_x, train_y, epochs=100, verbose=2)
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
+import src.core.dataAdapter as adapter
+import src.core.network as net
+# import sys
+# sys.path.append("..")
+# import core.dataAdapter
 
-saveModel(model)
+
+model = net.loadModel()
+train_x, train_y, test_x, test_y = adapter.loadMemory()
+ret = model.fit(train_x, train_y, epochs=100, verbose=2)
+net.saveModel(model)
