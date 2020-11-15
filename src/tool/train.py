eed2c0fe137c9ca6726bf16a2fed4a332e93c111
kcc5107 <kcc5107@naver.com>
2020-11-15 15:48:43 +0900

    데이터 학습 및 평가

--- src/tool/train.py
+++ src/tool/train.py
@@ -2,6 +2,7 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 import src.core.dataAdapter as adapter
 import src.core.network as net
+import matplotlib.pyplot as plt
 # 데이터 셔플이 필요시 주석 제거
 #import src.tool.fileclassification as fileclass
 
@@ -9,4 +10,12 @@ import src.core.network as net
 model = net.loadModel()
 train_x, train_y, test_x, test_y = adapter.loadMemory()
 ret = model.fit(train_x, train_y, epochs=100, verbose=2)
+loss = ret.history['loss']
+plt.plot(loss)
+plt.xlabel('epochs')
+plt.ylabel('loss')
+plt.show()
+
+test_loss, test_acc = model.evaluate(test_x,  test_y, verbose=2)
+print(test_acc)
 net.saveModel(model)
